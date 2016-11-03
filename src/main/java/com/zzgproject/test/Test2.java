d3144db8717b32be6e617c866dd1465d4d0fa97a
likeflower950 <likeflower950@163.com>
2016-11-03 15:44:41 +0800

    dateutil类进行修改完善

--- src/main/java/com/zzgproject/test/Test2.java
+++ src/main/java/com/zzgproject/test/Test2.java
@@ -1,10 +1,13 @@
 package com.zzgproject.test;
 
 import java.io.File;
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.alibaba.fastjson.JSONObject;
 
 public class Test2 {
 	
@@ -30,6 +33,27 @@ public class Test2 {
 		System.out.println(arr[arr.length-1].startsWith("."));
 		System.out.println(url.split("/."));
 		System.out.println(url.split("/.").length);
+		
+		JSONObject json = new JSONObject();
+		System.out.println(json);
+		json.put("abc", "22.22");
+		json.put("def", "00.50");
+		
+		BigDecimal sum = new BigDecimal("0.0000");
+		
+		System.out.println(sum);
+		
+		BigDecimal abc = json.getBigDecimal("abc");
+		BigDecimal def = json.getBigDecimal("def");
+		Double d1 = json.getDouble("abc");
+		Double d2 = json.getDoubleValue("def");
+		System.out.println(d1+d2);
+		System.out.println(abc);
+		System.out.println(def);
+		abc = abc.add(def);
+		System.out.println(abc);
+		
+		
 	}
 	
 	
--- src/main/java/com/zzgproject/utils/DateUtil.java
+++ src/main/java/com/zzgproject/utils/DateUtil.java
@@ -5,6 +5,9 @@ import java.util.Date;
 
 public class DateUtil {
 	
+    /**
+     * 根据默认的格式或者指定的格式返回当前时间带格式的时间字符串
+     */
 	public static String getDate(){
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		return sdf.format(new Date());
@@ -21,6 +24,10 @@ public class DateUtil {
 	}
 
 	
+	/**
+	 * ====================分割线==================
+	 * 返回给定long值时间的格式
+	 */
 	public static String getDate(long timer){
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		return sdf.format(new Date(timer));
@@ -31,15 +38,32 @@ public class DateUtil {
 		return sdf.format(new Date(timer));
 	}
 	
-	public static String getDate(Date timer){
+    public static String getDateFormat(long timer, String format){
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(new Date(timer));
+    }
+	
+	
+    /**
+     * ====================分割线==================
+     * 返回给定的date时间的格式
+     */
+	public static String getDate(Date date){
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		return sdf.format(timer);
+		return sdf.format(date);
 	}
 	
-	public static String getDatetime(Date timer){
+	public static String getDatetime(Date date){
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		return sdf.format(timer);
+		return sdf.format(date);
 	}
+	
+    public static String getDateFormat(Date date, String format){
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(date);
+    }
+	
+	
 }
 
 
