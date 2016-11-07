712d6e30dcbe64b823cebe34281917b626c998c7
likeflower950 <likeflower950@163.com>
2016-11-07 09:08:03 +0800

    文件工具类

---
+++ src/main/java/com/zzgproject/file/FileUtil.java
@@ -0,0 +1,70 @@
+package com.zzgproject.file;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+
+public class FileUtil {
+
+    
+    /**
+     * 
+     * 引入commons-io-xx.jar
+     * 使用包中封装的FileUtils工具类
+     * 
+     */
+    
+    public static void main(String[] args) throws IOException{
+        
+        String root = "E:" + File.separator;
+        
+        File dir = new File(root + "aaa");
+        System.out.println(dir.exists());
+        
+        /**
+         * FileUtils.cleanDirectory(dir) : 清空文件夹中的所有文件和文件夹
+         */
+        if(dir.exists()) FileUtils.cleanDirectory(dir);
+        
+        /**
+         * 使用JDK的File对象是无法直接删除文件夹的
+         * 
+         * FileUtils.deleteDirectory(dir) : 直接删除文件夹
+         */
+        if(dir.exists()) FileUtils.deleteDirectory(dir);
+        
+        /**
+         * FileUtils.deleteQuietly(dir) : 快速删除，不抛出任何异常
+         */
+        if(dir.exists()) FileUtils.deleteQuietly(dir);
+        
+        /**
+         * 读取文本文件
+         */
+        File file = new File(root + "代帐易.sql");
+        List<String> list = FileUtils.readLines(file, "GBK");
+        System.out.println(list);
+        
+        String str = FileUtils.readFileToString(file, "GBK");
+//        System.out.println(str);
+        
+        System.out.println(file.getPath());
+        
+        /**
+         * 复制文件以及复制文件夹
+         */
+        FileUtils.copyFile(file, new File(root + "abc.sql"));
+        
+        File srcDir = new File(root + "chromDownload");
+        File destDir = new File(root + "chromDownload2");
+//        FileUtils.copyDirectory(srcDir, destDir);
+        
+        FileUtils.copyFile(file, new File(root + "abc2.sql"));
+        
+        
+        System.out.println(FilenameUtils.getName(file.getPath()));
+    }
+}
--- src/main/java/com/zzgproject/test/Test2.java
+++ src/main/java/com/zzgproject/test/Test2.java
@@ -13,45 +13,6 @@ public class Test2 {
 	
 	public static void main(String[] args) throws Exception{
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		long sd = sdf.parse("2015-11-12").getTime();
-		long ed = sdf.parse("2015-11-12").getTime();
-		System.out.println(sd);
-		System.out.println(ed);
-		System.out.println(sd > ed);
-		
-		
-		File file = new File("D:\\mm\\mmmjing.txt");
-		System.out.println(file.exists());
-		System.out.println(file.getParent());
-		System.out.println(new File(file.getParent()).exists());
-		System.out.println( new File(file.getParent()).mkdirs() );
-		
-		String url = "http://tu.joy3g.com/newpic/1.jpg";
-		String[] arr = url.split("/.");
-		System.out.println(arr[arr.length-1]);
-		System.out.println(arr[arr.length-1].startsWith("."));
-		System.out.println(url.split("/."));
-		System.out.println(url.split("/.").length);
-		
-		JSONObject json = new JSONObject();
-		System.out.println(json);
-		json.put("abc", "22.22");
-		json.put("def", "00.50");
-		
-		BigDecimal sum = new BigDecimal("0.0000");
-		
-		System.out.println(sum);
-		
-		BigDecimal abc = json.getBigDecimal("abc");
-		BigDecimal def = json.getBigDecimal("def");
-		Double d1 = json.getDouble("abc");
-		Double d2 = json.getDoubleValue("def");
-		System.out.println(d1+d2);
-		System.out.println(abc);
-		System.out.println(def);
-		abc = abc.add(def);
-		System.out.println(abc);
 		
 		
 	}
